d2f8ed97b271af75f41bdd6f350bcd5b84a7cf2a
gonchaba <104991099+gonchaba@users.noreply.github.com>
2023-03-12 18:43:50 +0300

    Initial commit

--- src/main/java/com/gonchaba/codeassighnment/CodeAssignmentApplication.java
+++ src/main/java/com/gonchaba/codeassighnment/CodeAssignmentApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeAssignmentApplication {
+public class  CodeAssignmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeAssignmentApplication.class, args);
